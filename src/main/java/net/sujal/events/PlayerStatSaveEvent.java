c9b6d22985f1fa87100004a45f2cea2091996332
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-03 07:55:30 +0530

    Create PlayerStatSaveEvent.java

---
+++ src/main/java/net/sujal/events/PlayerStatSaveEvent.java
@@ -0,0 +1,24 @@
+package net.sujal.events;
+
+import net.sujal.data.PlayerData;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PlayerStatSaveEvent extends Event {
+    private static final HandlerList HANDLERS = new HandlerList();
+    private final Player player;
+    private final PlayerData playerData;
+
+    public PlayerStatSaveEvent(Player player, PlayerData playerData) {
+        this.player = player;
+        this.playerData = playerData;
+    }
+
+    public Player getPlayer() { return player; }
+    public PlayerData getPlayerData() { return playerData; }
+
+    @Override
+    public HandlerList getHandlers() { return HANDLERS; }
+    public static HandlerList getHandlerList() { return HANDLERS; }
+}
