f53b841ddc1c7243ec8fb1d9a07a051c0402a318
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-03 08:01:07 +0530

    Create DamageCalculator.java

---
+++ src/main/java/net/sujal/stats/DamageCalculator.java
@@ -0,0 +1,49 @@
+package net.sujal.stats;
+
+import net.sujal.SkyblockCore;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DamageCalculator {
+    private final SkyblockCore plugin;
+
+    public DamageCalculator(SkyblockCore plugin) {
+        this.plugin = plugin;
+    }
+
+    public double calculateDamage(Player attacker, LivingEntity victim, double baseWeaponDamage) {
+        UUID uuid = attacker.getUniqueId();
+        StatManager stats = plugin.getStatManager();
+        
+        double strength = stats.getFinalStat(uuid, StatType.STRENGTH);
+        double critChance = stats.getFinalStat(uuid, StatType.CRIT_CHANCE);
+        double critDamage = stats.getFinalStat(uuid, StatType.CRIT_DAMAGE);
+        
+        double baseMultiplier = plugin.getConfig().getDouble("settings.damage-multiplier", 5.0);
+        double damage = (baseWeaponDamage + baseMultiplier) * (1 + (strength / 100.0));
+        
+        boolean isCrit = ThreadLocalRandom.current().nextDouble(100.0) <= critChance;
+        if (isCrit) {
+            damage *= (1 + (critDamage / 100.0));
+        }
+
+        // Apply victim defense if victim is a player
+        if (victim instanceof Player pVictim) {
+            double defense = stats.getFinalStat(pVictim.getUniqueId(), StatType.DEFENSE);
+            double trueDefense = stats.getFinalStat(pVictim.getUniqueId(), StatType.TRUE_DEFENSE);
+            
+            // Standard skyblock armor formula
+            double damageReduction = defense / (defense + 100.0); 
+            damage *= (1.0 - damageReduction);
+            
+            if (trueDefense > 0) {
+                double trueReduction = trueDefense / (trueDefense + 100.0);
+                damage *= (1.0 - trueReduction);
+            }
+        }
+        
+        return damage;
+    }
+}
