de6ac59c8d9958126abb190599855c60f17d0995
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-03 13:48:22 +0530

    Update GuiProtectionListener.java

--- src/main/java/net/sujal/listeners/GuiProtectionListener.java
+++ src/main/java/net/sujal/listeners/GuiProtectionListener.java
@@ -9,15 +9,20 @@ import org.bukkit.inventory.InventoryHolder;
 
 public class GuiProtectionListener implements Listener {
 
-    @EventHandler
+        @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         InventoryHolder holder = event.getInventory().getHolder();
-        // Agar inventory hamari custom MenuHolder wali hai, toh click cancel kardo
+        
         if (holder instanceof MenuHolder) {
-            event.setCancelled(true);
+            event.setCancelled(true); // Steal karna mana hai
+            
+            // Agar player ne "Close" (Barrier) par click kiya
+            if (event.getCurrentItem() != null && event.getCurrentItem().getType() == org.bukkit.Material.BARRIER) {
+                event.getWhoClicked().closeInventory();
+            }
         }
     }
-
+    
     @EventHandler
     public void onInventoryDrag(InventoryDragEvent event) {
         InventoryHolder holder = event.getInventory().getHolder();
