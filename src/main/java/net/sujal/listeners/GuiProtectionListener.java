a8cf6bc5847019ffd29bb81b2a461daa1277832c
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-03 12:37:42 +0530

    Update GuiProtectionListener.java

--- src/main/java/net/sujal/listeners/GuiProtectionListener.java
+++ src/main/java/net/sujal/listeners/GuiProtectionListener.java
@@ -1,5 +1,6 @@
 package net.sujal.listeners;
 
+import net.sujal.gui.MenuHolder;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -8,23 +9,20 @@ import org.bukkit.inventory.InventoryHolder;
 
 public class GuiProtectionListener implements Listener {
 
-    // Isme hum future custom GUI menus ko protect kar rahe hain
-    // Taki koi item steal ya shift-click exploits na kar sake
-
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         InventoryHolder holder = event.getInventory().getHolder();
-        // Assuming future GUI menus implement a custom interface, e.g., 'CustomGUI'
-        // if (holder instanceof CustomGUI) {
-        //     event.setCancelled(true);
-        // }
+        // Agar inventory hamari custom MenuHolder wali hai, toh click cancel kardo
+        if (holder instanceof MenuHolder) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
     public void onInventoryDrag(InventoryDragEvent event) {
         InventoryHolder holder = event.getInventory().getHolder();
-        // if (holder instanceof CustomGUI) {
-        //     event.setCancelled(true);
-        // }
+        if (holder instanceof MenuHolder) {
+            event.setCancelled(true);
+        }
     }
 }
