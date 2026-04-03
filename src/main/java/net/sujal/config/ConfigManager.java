6ea3f1a51337f8b3642d92c82d36246d7a3bcdd2
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-03 13:52:45 +0530

    Update ConfigManager.java

--- src/main/java/net/sujal/config/ConfigManager.java
+++ src/main/java/net/sujal/config/ConfigManager.java
@@ -2,13 +2,21 @@ package net.sujal.config;
 
 import net.sujal.SkyblockCore;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ConfigManager {
 
     private final SkyblockCore plugin;
+    private File menusFile;
+    private FileConfiguration menusConfig;
 
     public ConfigManager(SkyblockCore plugin) {
         this.plugin = plugin;
+        plugin.saveDefaultConfig();
+        createMenusConfig();
     }
 
     public FileConfiguration getConfig() {
@@ -17,5 +25,19 @@ public class ConfigManager {
 
     public void reloadConfig() {
         plugin.reloadConfig();
+        createMenusConfig(); // Reload menus too
+    }
+
+    private void createMenusConfig() {
+        menusFile = new File(plugin.getDataFolder(), "menus.yml");
+        if (!menusFile.exists()) {
+            menusFile.getParentFile().mkdirs();
+            plugin.saveResource("menus.yml", false);
+        }
+        menusConfig = YamlConfiguration.loadConfiguration(menusFile);
+    }
+
+    public FileConfiguration getMenusConfig() {
+        return menusConfig;
     }
 }
