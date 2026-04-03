5653490bd9063ef86691d00004fbbd2831d9d1ef
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-03 08:09:18 +0530

    Create StatsCommand.java

---
+++ src/main/java/net/sujal/commands/StatsCommand.java
@@ -0,0 +1,27 @@
+package net.sujal.commands;
+
+import net.sujal.SkyblockCore;
+import net.sujal.gui.StatsGui;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class StatsCommand implements CommandExecutor {
+    private final SkyblockCore plugin;
+
+    public StatsCommand(SkyblockCore plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player player) {
+            StatsGui.open(player, plugin);
+            return true;
+        }
+        sender.sendMessage("Only players can use this command.");
+        return true;
+    }
+}
